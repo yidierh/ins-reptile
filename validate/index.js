4d5281cabf54c4b889db592b77f7beba1b9aef17
yidierh <yidierh@outlook.com>
2019-12-23 18:11:09 +0800

    优化校验

--- validate/index.js
+++ validate/index.js
@@ -7,12 +7,12 @@ export const validUrl = (rule, value, callback) => {
   if (!value) {
     return callback(new Error('url 不能为空！'))
   }
-  setTimeout(() => {
-    const reg =  new RegExp(/(ht|f)tp(s?)\:\/\/[0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*(:(0-9)*)*(\/?)([a-zA-Z0-9\-\.\?\,\'\/\\\+&amp;%\$#_]*)?/)
+  if (value) {
+    const reg = new RegExp(/(ht|f)tp(s?)\:\/\/[0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*(:(0-9)*)*(\/?)([a-zA-Z0-9\-\.\?\,\'\/\\\+&amp;%\$#_]*)?/)
     if (!reg.test(value)) {
       return callback(new Error('请检查地址是否输入有误'))
     } else {
       callback()
     }
-  }, 500)
+  }
 }

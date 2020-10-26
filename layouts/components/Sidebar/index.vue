55a5fbb2d5fb544d8a0ee2f7c58e1fa907f2d2eb
yidier <yidierh@gmail.com>
2020-10-26 23:44:52 +0800

    新增阿里巴巴

--- layouts/components/Sidebar/index.vue
+++ layouts/components/Sidebar/index.vue
@@ -7,6 +7,10 @@
       <span slot="title">INSTAGRAM</span>
     </el-menu-item>
     <el-menu-item index="2">
+      <i class="el-icon-shopping-bag-2"></i>
+      <span slot="title">ALIBABA</span>
+    </el-menu-item>
+    <el-menu-item index="3">
       <i class="el-icon-video-camera"></i>
       <span slot="title">YOUTUBE</span>
     </el-menu-item>
@@ -14,16 +18,22 @@
 </template>
 
 <script>
-  export default {
-    name: "Sidebar",
-    methods: {
-      selectHandler(index) {
-        if (index === '1') {
-          this.$router.push('/instagram')
-        } else {
-          this.$router.push('/youtube')
+    export default {
+        name: "Sidebar",
+        methods: {
+            selectHandler(index) {
+                switch (index) {
+                    case '1':
+                        this.$router.push('/instagram')
+                        break
+                    case '2':
+                        this.$router.push('/alibaba')
+                        break
+                    default:
+                        this.$router.push('/youtube')
+
+                }
+            }
         }
-      }
     }
-  }
 </script>
---
+++ pages/alibaba/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="alibaba-container">
+    ALIBABA
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "Alibaba"
+    }
+</script>
+
+<style rel="stylesheet/less" lang="less" scoped>
+  .alibaba-container {
+  }
+</style>
